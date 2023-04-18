88f445cd7db3ec6e06f124e5b7e20fba4475f311
Peter Pakanun <lpeachl@gmail.com>
2023-04-18 17:14:22 +0700

    add aw version

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const files = fs.readdirSync(path.join(__dirname, 'NeverSink-Filter')).filter(fi
 // load override file
 const override_1 = fs.readFileSync(path.join(__dirname, 'override_1.filter'), 'utf8')
 const override_2 = fs.readFileSync(path.join(__dirname, 'override_2.filter'), 'utf8')
+const override_aw = fs.readFileSync(path.join(__dirname, 'override_aw.filter'), 'utf8')
 
-// ensure build folder exist
-fs.ensureDirSync(path.join(__dirname, 'build'))
+// ensure build/normal and build/AW folder exist
+fs.ensureDirSync(path.join(__dirname, 'build', 'normal'))
+fs.ensureDirSync(path.join(__dirname, 'build', 'AW'))
 
 // loop into each files
 files.forEach(file => {
@@ -18,22 +20,32 @@ files.forEach(file => {
   const lineData = data.split('\r\n');
   // construct new file
   let newFileData = ''
+  let newFileDataAW = ''
 
   // add each line into the new file until we find the line that started with:
   // # !! Override 010 : "ALL Rules"
   // then we insert content of override_1.filter
   // # !! Override 020 : "influenced rules"
   // then we insert content of override_2.filter
+  // # !! Override 170 : "Overquality and endgame 4links"
+  // then we insert content of override_aw.filter but only for the AW version
   for (let i = 0; i < lineData.length; i++) {
     newFileData += lineData[i] + '\r\n'
+    newFileDataAW += lineData[i] + '\r\n'
     if (lineData[i].startsWith('# !! Override 010 : "ALL Rules"')) {
       newFileData += '\r\n' + override_1 + '\r\n'
+      newFileDataAW += '\r\n' + override_1 + '\r\n'
     }
     if (lineData[i].startsWith('# !! Override 020 : "influenced rules"')) {
       newFileData += '\r\n' + override_2 + '\r\n'
+      newFileDataAW += '\r\n' + override_2 + '\r\n'
+    }
+    if (lineData[i].startsWith('# !! Override 170 : "Overquality and endgame 4links"')) {
+      newFileDataAW += '\r\n' + override_aw + '\r\n'
     }
   }
   
   // write new file
-  fs.writeFileSync(path.join(__dirname, 'build', file), newFileData)
+  fs.writeFileSync(path.join(__dirname, 'build', 'normal', file), newFileData)
+  fs.writeFileSync(path.join(__dirname, 'build', 'AW', file), newFileDataAW)
 });
